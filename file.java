e5a8170a688d4e629677d10ea716f4014f320ca6
Gemimah <149108553+Gemimah@users.noreply.github.com>
2024-09-09 14:44:41 +0200

    Add files via upload

---
+++ file.java
@@ -0,0 +1,17 @@
+// First program
+class Hello2 { 
+    public static void main(String[] arguments) {
+        System.out.println("Hello world");
+        System.out.println("Line number 2");
+    }
+}
+
+class Hello3 {
+    public static void main(String[] arguments) {
+        String foo ="IAP 6.092";
+            System.out.println(foo);
+            foo="Something else";
+            System.out.println(foo);
+
+    }
+}
