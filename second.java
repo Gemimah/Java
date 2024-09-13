52f76e7cc1c02901572a1b1b1c29c376a5ad240e
Gemimah <149108553+Gemimah@users.noreply.github.com>
2024-09-13 23:11:01 +0200

    Add files via upload

---
+++ second.java
@@ -0,0 +1,11 @@
+ class second {
+    public static void main(String[] args) {
+        Student myObj = new Student();
+        System.out.println("Name: " + myObj.fname);
+        System.out.println("age:" + myObj.age);
+        System.out.println("graduationYear :" + myObj.graduationYear);
+        myObj.study();
+
+    }
+    
+}
